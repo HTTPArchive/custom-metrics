53f58c5422911367170124b8240e2aec23d728c1
csliva <colt.sliva@gmail.com>
2022-05-17 17:07:48 -0400

    verify head only contains valid elements

---
+++ dist/valid-head.js
@@ -0,0 +1,7 @@
+var valid_elements = ['TITLE', 'META', 'LINK', 'SCRIPT', 'STYLE', 'BASE', 'NOSCRIPT', 'TEMPLATE']
+var elems = document.head.getElementsByTagName("*");
+for (var i=0; i<elems.length;i++) {
+    if (!valid_elements.includes(elems[i].tagName)) {
+        return true;
+    };
+}
