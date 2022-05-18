b701c9c32f161fe8a3cc70e0b5c004cb2c14aeb0
csliva <colt.sliva@gmail.com>
2022-05-18 12:22:19 -0400

    Using WPT_BODIES and XML Parser to extract head elements

--- dist/valid-head.js
+++ dist/valid-head.js
@@ -1,8 +1,26 @@
-var valid_elements = ['TITLE', 'META', 'LINK', 'SCRIPT', 'STYLE', 'BASE', 'NOSCRIPT', 'TEMPLATE']
-var elems = document.head.getElementsByTagName("*");
-for (var i=0; i<elems.length;i++) {
-    if (!valid_elements.includes(elems[i].tagName)) {
-        return true;
+// Used for WPT 
+// ['valid-head'] 
+
+// Use DOMParser to extract head elements so browser can't truncate the head
+// Loop through each node and check against valid elements array
+// Returns object with ARRAY and BOOL
+var html = $WPT_BODIES[0].response_body;
+var parser = new DOMParser();
+var xmlDoc = parser.parseFromString(html,"text/xml");
+var head = xmlDoc.getElementsByTagName("head")[0]
+var nodes = head.getElementsByTagName("*")
+
+let data = {
+       "invalidElements":  [],
+       "invalidHead": false
+}
+
+var valid_elements = ['title', 'meta', 'link', 'script', 'style', 'base', 'noscript', 'template']
+
+for (var i=0; i<nodes.length;i++) {
+    if (!valid_elements.includes(nodes[i].tagName)) {
+       data.invalidElements.push(nodes[i].tagName);
+       data.invalidHead = true
     };
 }
-return false
+return data
