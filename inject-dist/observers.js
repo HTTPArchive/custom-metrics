272042d65695ce84e34313624db9688bd15fbf28
Rick Viscomi <rviscomi@users.noreply.github.com>
2022-05-19 15:03:44 -0400

    Update observers.js

--- inject-dist/observers.js
+++ inject-dist/observers.js
@@ -1,10 +1,10 @@
-observers = {};
+const httparchive_observers = {};
 
-init = {navigator: {userAgent: navigator.userAgent}};
+const httparchive_init = {navigator: {userAgent: navigator.userAgent}};
 
 Object.defineProperty(navigator, 'userAgent', {
     get: () => {
-        observers.userAgent = (observers.userAgent || 0) + 1;
-        return init.navigator.userAgent;
+        httparchive_observers.userAgent = (httparchive_observers.userAgent || 0) + 1;
+        return httparchive_init.navigator.userAgent;
     }
 })
