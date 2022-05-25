4f4a540f5956f1d56a3d96eef788a8efce0643a6
csliva <colt.sliva@gmail.com>
2022-05-25 18:12:16 -0400

    try catch, case handling, and parseerror element handling

--- dist/valid-head.js
+++ dist/valid-head.js
@@ -3,21 +3,37 @@
 // Use DOMParser to extract head elements so browser can't truncate the head
 // Loop through each node and check against valid elements array
 // Returns object with ARRAY and BOOL
-var html = $WPT_BODIES[0].response_body;
-var parser = new DOMParser();
-var xmlDoc = parser.parseFromString(html,"text/xml");
-var head = xmlDoc.getElementsByTagName("head")[0]
-var nodes = head.getElementsByTagName("*")
+try {
+    var html = $WPT_BODIES[0].response_body;
+    var parser = new DOMParser();
+    var xmlDoc = parser.parseFromString(html,"text/xml");
 
-let data = {
-    "invalidElements":  [],
-    "invalidHead": false
+    // remove <parsererror> because HTML is not really XML
+    let pe = xmlDoc.getElementsByTagName("parsererror")
+    if (pe.length > 0) {
+        pe[0].remove()
+    }
+    
+    var head = xmlDoc.getElementsByTagName("head")[0]
+    var nodes = head.getElementsByTagName("*")
+    
+
+    let data = {
+        "invalidElements":  [],
+        "invalidHead": false
+    }
+    var valid_elements = ['title', 'meta', 'link', 'script', 'style', 'base', 'noscript', 'template']
+    for (var i=0; i<nodes.length;i++) {
+        let tagname = nodes[i].tagName.toLowerCase()
+        if (!valid_elements.includes(tagname)) {
+            data.invalidElements.push(nodes[i].tagName);
+            data.invalidHead = true
+        };
+    }
+    return data
+} catch (err) {
+    console.log(err);
+  return {
+      error: err.toString()
+  }
 }
-var valid_elements = ['title', 'meta', 'link', 'script', 'style', 'base', 'noscript', 'template']
-for (var i=0; i<nodes.length;i++) {
-    if (!valid_elements.includes(nodes[i].tagName)) {
-        data.invalidElements.push(nodes[i].tagName);
-        data.invalidHead = true
-    };
-}
-return data
