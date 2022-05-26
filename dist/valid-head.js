7f702ea8c115c47f3871cff720582efe9f3c3c73
csliva <colt.sliva@gmail.com>
2022-05-26 18:01:58 -0400

    Removing XML implementation due to number of edge cases. Replacing with surrogate HTML element.

--- dist/valid-head.js
+++ dist/valid-head.js
@@ -1,38 +1,36 @@
 // Used for WPT
-// [valid-head]
+//[valid-head]
 // Use DOMParser to extract head elements so browser can't truncate the head
 // Loop through each node and check against valid elements array
 // Returns object with ARRAY and BOOL
 try {
     var html = $WPT_BODIES[0].response_body;
-    var parser = new DOMParser();
-    var xmlDoc = parser.parseFromString(html,"text/xml");
+    // create in interrupt element which prevents truncation.
+    html = html.replace('</head>', "<interrupt />\n</head>");
 
-    // remove <parsererror> because HTML is not really XML
-    let pe = xmlDoc.getElementsByTagName("parsererror")
-    if (pe.length > 0) {
-        pe[0].remove()
-    }
+    var doc = document.implementation.createHTMLDocument("New Document"); 
+    var head = doc.createElement("div");
+    head.innerHTML = html;
 
-    var head = xmlDoc.getElementsByTagName("head")[0]
-    var nodes = head.getElementsByTagName("*")
+    [...head.getElementsByTagName("interrupt")].forEach(el => el.remove());
+    var nodes = head.getElementsByTagName("*");
 
     let data = {
         "invalidElements":  [],
         "invalidHead": false
     }
     var valid_elements = ['title', 'meta', 'link', 'script', 'style', 'base', 'noscript', 'template']
-    for (var i=0; i<nodes.length;i++) {
+        for (var i=0; i<nodes.length;i++) {
         let tagname = nodes[i].tagName.toLowerCase()
         if (!valid_elements.includes(tagname)) {
-            data.invalidElements.push(nodes[i].tagName);
-            data.invalidHead = true
+            data.invalidElements.push(tagname);
+            data.invalidHead = true;
         };
     }
-    return data
+    return data;
 } catch (err) {
     console.log(err);
-  return {
-      error: err.toString()
+  {
+      return {error: err.toString()};
   }
 }
