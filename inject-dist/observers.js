60e4c00d301ff16b07dd7be1e54939d25edd6811
Rick Viscomi <rviscomi@users.noreply.github.com>
2022-05-27 12:17:15 -0400

    eval, DEBUG_MODE, and a fix for top-level props

--- inject-dist/observers.js
+++ inject-dist/observers.js
@@ -8,6 +8,9 @@ let httparchive_enable_observations = false;
 // Local scope.
 (() => {
 
+// Additional logging.
+const DEBUG_MODE = false;
+
 // Add the pathnames of any functions/properties you want to observe.
 const OBSERVERS = [
   'navigator.__proto__.*',
@@ -22,7 +25,8 @@ const OBSERVERS = [
   'queueMicrotask',
   'requestIdleCallback',
   'navigator.scheduling.isInputPending',
-  'scheduler.postTask'
+  'scheduler.postTask',
+  'eval'
 ];
 
 const PROPERTIES_TO_TRACE = new Set([
@@ -55,9 +59,16 @@ function getAllProperties(pathname, depth=1) {
   const parentPathname = props.slice(0, -1).join('.');
   const parentObj = resolveObject(parentPathname);
 
-  return Object.getOwnPropertyNames(parentObj).flatMap(prop => {
-    return getAllProperties(`${parentPathname}.${prop}`, depth - 1);
-  });
+  try {
+    return Object.getOwnPropertyNames(parentObj).flatMap(prop => {
+      return getAllProperties(`${parentPathname}.${prop}`, depth - 1);
+    });
+  } catch (e) {
+    if (DEBUG_MODE) {
+      console.debug(`Cannot get property names of ${parentPathname}, parent object: ${parentObj}, error: ${e}`);
+    }
+    return pathname;
+  }
 }
 
 function initializeObserver(pathname) {
@@ -73,13 +84,19 @@ function initializeObserver(pathname) {
     parentPathname = props.slice(0, -1).join('.');
   }
 
-  pathname = `${parentPathname}.${prop}`;
-  const parentObj = resolveObject(parentPathname);
+  let parentObj = window;
+  if (parentPathname) {
+    pathname = `${parentPathname}.${prop}`;
+    parentObj = resolveObject(parentPathname);
+  }
 
   try {
     original = parentObj[prop];
   } catch (e) {
     // The property is not accessible.
+    if (DEBUG_MODE) {
+      console.debug(`${pathname} is not accessible: ${e}`)
+    }
     return;
   }
 
@@ -115,6 +132,9 @@ function initializeObserver(pathname) {
     });
   } catch (e) {
     // The property is not observable.
+    if (DEBUG_MODE) {
+      console.debug(`${pathname} is not observable: ${e}`)
+    }
     return;
   }
 
