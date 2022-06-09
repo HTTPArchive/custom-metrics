48865343b15d898b3c472d72a1327e50333f24cc
Rick Viscomi <rviscomi@users.noreply.github.com>
2022-06-08 20:32:35 -0400

    Disable observers.js (#43)
    Update observers.js

--- dist/observers.js
+++ dist/observers.js
@@ -2,4 +2,5 @@
 // Reads the global httparchive_observers from the injected script.
 // See https://github.com/HTTPArchive/custom-metrics/tree/main/inject-dist
 
-return Object.fromEntries(Object.entries(httparchive_observers).filter(([_, value]) => value));
+//return Object.fromEntries(Object.entries(httparchive_observers).filter(([_, value]) => value));
+return null;
