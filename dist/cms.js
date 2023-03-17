ee5e552907a6010bdbf616d5ce9a1fe6e1a01945
Adam Silverstein <adamsilverstein@earthboundhosting.com>
2023-03-17 12:50:48 -0600

    Add WordPress embed block detection (#73)
    * Add WordPress oEmbed block detection
    
    * Add has_embed_block to returned data
    
    * Detect figure, move detection funciton up
    
    * restore end of file empty line

--- dist/cms.js
+++ dist/cms.js
@@ -4,8 +4,14 @@ function usesBlockTheme() {
   return !!document.querySelector('div.wp-site-blocks');
 }
 
+// Detects if a WordPress embed block is on the page
+function hasWordPressEmbedBlock() {
+  return !!document.querySelector('figure.wp-block-embed');
+}
+
 const wordpress = {
-  block_theme: usesBlockTheme()
+  block_theme: usesBlockTheme(),
+  has_embed_block: hasWordPressEmbedBlock()
 };
 
 return {
