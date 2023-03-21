f824af8f5568f70c649adfeb83292a9c87ff98ca
Adam Silverstein <adamjs@google.com>
2023-03-21 14:37:33 -0600

    CMS: Add WordPress embed block counts

--- dist/cms.js
+++ dist/cms.js
@@ -9,9 +9,26 @@ function hasWordPressEmbedBlock() {
   return !!document.querySelector('figure.wp-block-embed');
 }
 
+// Count the number of WordPress embed blocks on the page, including a breakdown by type
+function getWordPressEmbedBlockCounts() {
+  const embedBlocks = document.querySelectorAll('figure.wp-block-embed');
+  const embedsByType = [];
+  for ( let embed of embedBlocks ) {
+    let provider = embed.className.split( ' ' ).pop().split( "-" ).pop();
+    embedsByType[ provider ] = embedsByType[ provider ] || 0;
+    embedsByType[ provider ]++;
+  }
+
+  return {
+    total: embedBlocks.length,
+    total_by_type: embedsByType
+  }
+}
+
 const wordpress = {
   block_theme: usesBlockTheme(),
-  has_embed_block: hasWordPressEmbedBlock()
+  has_embed_block: hasWordPressEmbedBlock(),
+  embed_block_count: getWordPressEmbedBlockCounts()
 };
 
 return {
