20978cf149d4d23a3b4b58fa9570a6ec2865654b
Weston Ruter <westonruter@google.com>
2023-07-06 17:04:00 -0700

    Add custom metrics for WordPress scripts

--- dist/cms.js
+++ dist/cms.js
@@ -35,10 +35,52 @@ function getWordPressEmbedBlockCounts() {
   }
 }
 
+/**
+ * Obtains data about scripts that WordPress prints on the page.
+ *
+ * @returns {{}[]}
+ */
+function getWordPressScripts() {
+  const entries = [];
+
+  /**
+   * Checks whether the provided element is an inline script.
+   *
+   * @param {?Element} element Element to examine. May be null.
+   * @return {?number} Size of the inline script or null if the element isn't an inline script.
+   */
+  const getInlineScriptSize = (element) => {
+    if (element instanceof HTMLScriptElement && !element.src) {
+      return element.textContent.length;
+    }
+    return null;
+  };
+
+  const scripts = document.querySelectorAll('script[src][id$="-js"]');
+  for (const script of scripts) {
+    /** @var HTMLScriptElement script */
+    const handle = script.id.replace(/-js$/, '');
+    entries.push({
+      handle,
+      src: script.src,
+      in_footer: script.parentNode !== document.head,
+      async: script.async,
+      defer: script.defer,
+      intended_strategy: script.dataset.wpStrategy || null,
+      after_script_size: getInlineScriptSize(document.getElementById(`${handle}-js-after`)),
+      before_script_size: getInlineScriptSize(document.getElementById(`${handle}-js-before`)),
+      extra_script_size: getInlineScriptSize(document.getElementById(`${handle}-js-extra`)),
+      translations_script_size: getInlineScriptSize(document.getElementById(`${handle}-js-translations`)),
+    });
+  }
+  return entries;
+}
+
 const wordpress = {
   block_theme: usesBlockTheme(),
   has_embed_block: hasWordPressEmbedBlock(),
-  embed_block_count: getWordPressEmbedBlockCounts()
+  embed_block_count: getWordPressEmbedBlockCounts(),
+  scripts: getWordPressScripts(),
 };
 
 return {
