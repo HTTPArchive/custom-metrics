5bb5d2ab35b3bc60ce171df2eb633174ee7b94d7
max-ostapenko <1611259+max-ostapenko@users.noreply.github.com>
2023-08-04 03:10:36 +0200

    review suggestions

--- tests/wpt.js
+++ tests/wpt.js
@@ -2,12 +2,12 @@ const WebPageTest = require('webpagetest');
 const fs = require('fs');
 
 const wptServer = 'www.webpagetest.org';
-let wptApiKey = process.env.WPT_API_KEY;
-let wpt = new WebPageTest(wptServer, wptApiKey);
+const wptApiKey = process.env.WPT_API_KEY;
+const wpt = new WebPageTest(wptServer, wptApiKey);
 
 function runWPTTest(url, options = {}) {
   options.f = 'json';
-  let customMetricsFile = './dist/' + options.label + '.js';
+  const customMetricsFile = `./dist/${options.label}.js`;
 
   // check that custom metrics file exists
   if (!fs.existsSync(customMetricsFile)) {
@@ -21,12 +21,12 @@ function runWPTTest(url, options = {}) {
     wpt.runTestAndWait(url, options, (error, response) => {
       console.log(`Running ${options.label} tests for ${url}`);
       if (error || response.statusCode !== 200) {
-        console.error(`Test for ${url} failed`);
+        console.error(`Test for ${url} failed:`);
         console.error(error || response);
         reject(error || response);
       } else {
-        console.log(`Test for ${url} succeeded`);
-        console.log(response);
+        console.log(`Test for ${url} succeeded. View full test results:`);
+        console.log(response.data.summary);
         console.log(`Custom metrics data:`);
         console.log(response.data.runs[1].firstView["_" + options.label]);
         resolve(JSON.parse(response.data.runs[1].firstView["_" + options.label]));
