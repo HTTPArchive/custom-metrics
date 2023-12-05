ace4a3f10c064dedc30f11e8fd16f5a9215b1731
Patrick Meenan <pmeenan@chromium.org>
2023-12-05 10:17:41 -0500

    Fail the test if the test redirected to a cross-origin page (#102)

---
+++ dist/test_result.js
@@ -0,0 +1,16 @@
+// Fails the test if the resulting page was on a different origin from where the test started
+const sameOrigin = function(uri1, uri2){
+  try {
+      uri1 = new URL(uri1);
+      uri2 = new URL(uri2);
+      return uri1.origin == uri2.origin;
+  } catch(e) {
+  }
+  return false;
+}
+const testUrl = $WPT_TEST_URL;
+const currentUrl = document.location.href;
+if (sameOrigin(testUrl, currentUrl)) {
+  return 0;  // No change
+}
+return 888;
