86f36ce3b1074dec581cd2aa042cccf3813675b7
Kevin Farrugia <hello@imkev.dev>
2024-01-18 16:59:39 +0100

    Add custom metric to measure generated content (size and percent)

---
+++ dist/generated-content.js
@@ -0,0 +1,19 @@
+let generatedContentPercent = 0;
+let generatedContent = 0;
+
+const byteSize = (str) => new Blob([str]).size;
+
+let htmlInitial = $WPT_BODIES[0].response_body;
+let htmlAfter = document.documentElement.outerHTML;
+
+if (htmlInitial && htmlAfter) {
+  let htmlInitialSize = byteSize(htmlInitial);
+  let htmlAfterSize = byteSize(htmlAfter);
+  generatedContentPercent = 100 - (htmlInitialSize / htmlAfterSize) * 100;
+  generatedContent = (htmlAfterSize - htmlInitialSize) / 1024;
+}
+
+return { 
+  percent: generatedContentPercent.toFixed(2),
+  sizeInKB: generatedContent.toFixed(2)
+}
