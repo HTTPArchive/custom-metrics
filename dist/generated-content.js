a2210b07ea9f9257cd9715e38759d880e3ba5f83
Kevin Farrugia <kevinfarrugia@users.noreply.github.com>
2024-01-19 09:31:52 +0100

    Apply suggestions from code review
    Co-authored-by: Rick Viscomi <rviscomi@users.noreply.github.com>

--- dist/generated-content.js
+++ dist/generated-content.js
@@ -9,11 +9,11 @@ let htmlAfter = document.documentElement.outerHTML;
 if (htmlInitial && htmlAfter) {
   let htmlInitialSize = byteSize(htmlInitial);
   let htmlAfterSize = byteSize(htmlAfter);
-  generatedContentPercent = 100 - (htmlInitialSize / htmlAfterSize) * 100;
+  generatedContentPercent = 1 - (htmlInitialSize / htmlAfterSize);
   generatedContent = (htmlAfterSize - htmlInitialSize) / 1024;
 }
 
-return JSON.stringify({
-  percent: generatedContentPercent.toFixed(2),
+return {
+  percent: generatedContentPercent.toFixed(4),
   sizeInKB: generatedContent.toFixed(2),
-});
+};
