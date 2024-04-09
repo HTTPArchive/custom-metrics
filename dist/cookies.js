c6272182d2611d433820ca5f3b2559c5a7d3fb57
Rick Viscomi <rviscomi@google.com>
2024-04-09 12:46:46 -0400

    http cookies

--- dist/cookies.js
+++ dist/cookies.js
@@ -4,12 +4,28 @@ function getCookieStore() {
   return cookieStore.getAll();
 }
 
+function getHTTPCookies() {
+  const requests = $WPT_REQUESTS;
+  return requests.filter(r => {
+    return r.response_headers['set-cookie']
+  }).map(r => ({
+    url: r.url,
+    response_header: r.response_headers['set-cookie'],
+    directives: Object.fromEntries(r.response_headers['set-cookie'].split(/;\s*/).map(directive => {
+      return directive.split('=');
+    }))
+  }));
+}
+
 return Promise.all([
   getCookieStore(),
 ]).then(([
   cookie_store
 ]) => {
+  const response_headers = getHTTPCookies();
+
   return {
     cookie_store,
+    response_headers
   };
 });
