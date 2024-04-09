67d6d56dc2b5a8e4e6469d87e42cc95162ce148a
Rick Viscomi <rviscomi@google.com>
2024-04-09 16:48:21 -0400

    handle multiple headers per response, handle boolean directives

--- dist/cookies.js
+++ dist/cookies.js
@@ -6,15 +6,40 @@ function getCookieStore() {
 
 function getHTTPCookies() {
   const requests = $WPT_REQUESTS;
-  return requests.filter(r => {
-    return r.response_headers['set-cookie']
-  }).map(r => ({
-    url: r.url,
-    response_header: r.response_headers['set-cookie'],
-    directives: Object.fromEntries(r.response_headers['set-cookie'].split(/;\s*/).map(directive => {
-      return directive.split('=');
-    }))
-  }));
+  return requests.filter(request => {
+    return 'set-cookie' in (request.response_headers ?? {})
+  }).flatMap(request => {
+    const values = request.response_headers['set-cookie'];
+    // There may be multiple Set-Cookie headers delimited by newlines.
+    return values.split('\n').map(value => {
+      let cookie_name = null;
+      let cookie_value = null;
+      // Directives are delimited by semicolons.
+      const directives =  Object.fromEntries(value.split(/;\s*/).map((directive, i) => {
+        // Key/value pairs are delimited by equals signs.
+        const [key, value] = directive.split('=');
+        if (i == 0) {
+          // The first directive is the cookie name and value.
+          [cookie_name, cookie_value] = [key, value ?? null];
+        }
+        // Use nullish coalescing to ensure that boolean directives are not omitted from the directive object.
+        // Examples: HttpOnly, Secure, and Partitioned.
+        return [key, value ?? null];
+      }));
+
+      return {
+        request_id: request.id,
+        request_url: request.url,
+        cookie_name,
+        cookie_value,
+        response_header: value,
+        directives,
+        http_only: 'HttpOnly' in directives,
+        secure: 'Secure' in directives,
+        partitioned: 'Partitioned' in directives,
+      };
+    });
+  });
 }
 
 return Promise.all([
