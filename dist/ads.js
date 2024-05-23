8d7c12b7a449a22071b0d411aba99c4e9ede4f2f
Yash Vekaria <yvekaria.09@gmail.com>
2024-05-23 14:03:31 -0700

    Adjusting left spacing

--- dist/ads.js
+++ dist/ads.js
@@ -6,188 +6,188 @@ const SELLER_TYPES = ['publisher', 'intermediary', 'both'];
 const isPresent = (response, endings) => response.ok && endings.some(ending => response.url.endsWith(ending));
 
 const fetchAndParse = async (url, parser) => {
-	const timeout = 5000;
-	/*
-	Google's sellers.json size is 120Mb as of May 2024 - too big for custom metrics.
-	It's available at realtimebidding.google.com/sellers.json, so not part of crawled pages list.
-	More details: https://support.google.com/authorizedbuyers/answer/9895942
-	*/
-	const controller = new AbortController();
-	const { signal } = controller;
-	setTimeout(() => controller.abort(), timeout);
+  const timeout = 5000;
+  /*
+  Google's sellers.json size is 120Mb as of May 2024 - too big for custom metrics.
+  It's available at realtimebidding.google.com/sellers.json, so not part of crawled pages list.
+  More details: https://support.google.com/authorizedbuyers/answer/9895942
+  */
+  const controller = new AbortController();
+  const { signal } = controller;
+  setTimeout(() => controller.abort(), timeout);
 
-	try {
-		const response = await fetch(url, { signal });
-		return parser(response);
-	} catch (error) {
-		return {
-			status: -1,
-			present: false,
-			error: error.message
-		};
-	}
+  try {
+    const response = await fetch(url, { signal });
+    return parser(response);
+  } catch (error) {
+    return {
+      status: -1,
+      present: false,
+      error: error.message
+    };
+  }
 };
 
 // https://iabtechlab.com/wp-content/uploads/2022/04/Ads.txt-1.1.pdf
 const parseAdsTxt = async (response) => {
-	let content = await response.text();
+  let content = await response.text();
 
-	let result = {
-		present: isPresent(response, ['/ads.txt', '/app-ads.txt']),
-		status: response.status,
-		redirected: response.redirected,
-	};
+  let result = {
+    present: isPresent(response, ['/ads.txt', '/app-ads.txt']),
+    status: response.status,
+    redirected: response.redirected,
+  };
 
-	if (result.present && content) {
-		result = {
-			...result,
-			...{
-				account_count: 0,
-				account_types: {
-					direct: {
-						domains: new Set(),
-						account_count: 0,
-					},
-					reseller: {
-						domains: new Set(),
-						account_count: 0,
-					}
-				},
-				line_count: 0,
-				variables: new Set(),
-				variable_count: 0
-			}
-		};
+  if (result.present && content) {
+    result = {
+      ...result,
+      ...{
+        account_count: 0,
+        account_types: {
+          direct: {
+            domains: new Set(),
+            account_count: 0,
+          },
+          reseller: {
+            domains: new Set(),
+            account_count: 0,
+          }
+        },
+        line_count: 0,
+        variables: new Set(),
+        variable_count: 0
+      }
+    };
 
-		// Clean up file content
-		content = content.replace(/#.*$/gm, '');
-		content = content.replace(/\r/g, '');
+    // Clean up file content
+    content = content.replace(/#.*$/gm, '');
+    content = content.replace(/\r/g, '');
 
-		let lines = content.split('\n');
-		result.line_count = lines.length;
+    let lines = content.split('\n');
+    result.line_count = lines.length;
 
-		for (let line of lines) {
-			// Variables
-			let variables = line.split('=');
-			if (variables.length == 2) {
-				result.variables.add(variables[0].trim().toLowerCase());
-				result.variable_count += 1;
-				continue;
-			}
+    for (let line of lines) {
+      // Variables
+      let variables = line.split('=');
+      if (variables.length == 2) {
+        result.variables.add(variables[0].trim().toLowerCase());
+        result.variable_count += 1;
+        continue;
+      }
 
-			// Account records
-			let relation_parts = line.split(',');
-			if (relation_parts.length >= 3) {
-				let type = relation_parts[2].trim().toLowerCase();
-				if (['direct', 'reseller'].includes(type)) {
-					result.account_types[type].domains.add(relation_parts[0].trim());
-					result.account_types[type].account_count += 1;
-				}
-				result.account_count += 1;
-			}
-		};
+      // Account records
+      let relation_parts = line.split(',');
+      if (relation_parts.length >= 3) {
+        let type = relation_parts[2].trim().toLowerCase();
+        if (['direct', 'reseller'].includes(type)) {
+          result.account_types[type].domains.add(relation_parts[0].trim());
+          result.account_types[type].account_count += 1;
+        }
+        result.account_count += 1;
+      }
+    };
 
-		// Count unique and remove domain Sets for now
-		for (let accountType of Object.values(result.account_types)) {
-			accountType.domain_count = accountType.domains.size;
-			accountType.domains = Array.from(accountType.domains); // delete accountType.domains
-		}
-		result.variables = Array.from(result.variables);
-	}
+    // Count unique and remove domain Sets for now
+    for (let accountType of Object.values(result.account_types)) {
+      accountType.domain_count = accountType.domains.size;
+      accountType.domains = Array.from(accountType.domains); // delete accountType.domains
+    }
+    result.variables = Array.from(result.variables);
+  }
 
-	return result;
+  return result;
 
 }
 
 
 // https://iabtechlab.com/wp-content/uploads/2019/07/Sellers.json_Final.pdf
 const parseSellersJSON = async (response) => {
-	let content;
-	try {
-		content = JSON.parse(await response.text());
-	} catch {
-		content = null;
-	}
-	let result = {
-		present: isPresent(response, ['/sellers.json']),
-		redirected: response.redirected,
-		status: response.status,
-	};
+  let content;
+  try {
+    content = JSON.parse(await response.text());
+  } catch {
+    content = null;
+  }
+  let result = {
+    present: isPresent(response, ['/sellers.json']),
+    redirected: response.redirected,
+    status: response.status,
+  };
 
-	if (result.present && content) {
-		result = {
-			...result,
-			...{
-				seller_count: 0,
-				seller_types: {
-					publisher: {
-						domains: new Set(),
-						seller_count: 0,
-					},
-					intermediary: {
-						domains: new Set(),
-						seller_count: 0,
-					},
-					both: {
-						domains: new Set(),
-						seller_count: 0,
-					}
-				},
-				passthrough_count: 0,
-				confidential_count: 0
-			}
-		};
+  if (result.present && content) {
+    result = {
+      ...result,
+      ...{
+        seller_count: 0,
+        seller_types: {
+          publisher: {
+            domains: new Set(),
+            seller_count: 0,
+          },
+          intermediary: {
+            domains: new Set(),
+            seller_count: 0,
+          },
+          both: {
+            domains: new Set(),
+            seller_count: 0,
+          }
+        },
+        passthrough_count: 0,
+        confidential_count: 0
+      }
+    };
 
-		// Clean up file content
-		result.seller_count = content.sellers.length;
+    // Clean up file content
+    result.seller_count = content.sellers.length;
 
-		for (let seller of content.sellers) {
-			const stype = seller.seller_type.trim().toLowerCase();
-			// Validating records
-			if (!SELLER_TYPES.includes(stype) || !seller.seller_id) {
-				continue;
-			}
+    for (let seller of content.sellers) {
+      const stype = seller.seller_type.trim().toLowerCase();
+      // Validating records
+      if (!SELLER_TYPES.includes(stype) || !seller.seller_id) {
+        continue;
+      }
 
-			// Passthrough
-			if (seller.is_passthrough) {
-				result.passthrough_count += 1;
-			}
+      // Passthrough
+      if (seller.is_passthrough) {
+        result.passthrough_count += 1;
+      }
 
-			// Confidential
-			if (seller.is_confidential) {
-				result.confidential_count += 1;
-			}
+      // Confidential
+      if (seller.is_confidential) {
+        result.confidential_count += 1;
+      }
 
-			// Seller records
-			if (seller.domain) {
-				const domain = seller.domain.trim().toLowerCase();
-				result.seller_types[stype].domains.add(domain);
-				result.seller_types[stype].seller_count += 1;
-			}
-		}
+      // Seller records
+      if (seller.domain) {
+        const domain = seller.domain.trim().toLowerCase();
+        result.seller_types[stype].domains.add(domain);
+        result.seller_types[stype].seller_count += 1;
+      }
+    }
 
-		// Count unique and remove domain Sets for now
-		for (let seller_type of Object.values(result.seller_types)) {
-			seller_type.domain_count = seller_type.domains.size;
-			seller_type.domains = Array.from(seller_type.domains); // delete seller_type.domains;
-		}
-	};
+    // Count unique and remove domain Sets for now
+    for (let seller_type of Object.values(result.seller_types)) {
+      seller_type.domain_count = seller_type.domains.size;
+      seller_type.domains = Array.from(seller_type.domains); // delete seller_type.domains;
+    }
+  };
 
-	return result;
+  return result;
 }
 
 return Promise.all([
-	fetchAndParse("/ads.txt", parseAdsTxt).catch(e => e),
-	fetchAndParse("/app-ads.txt", parseAdsTxt).catch(e => e),
-	fetchAndParse("/sellers.json", parseSellersJSON).catch(e => e),
+  fetchAndParse("/ads.txt", parseAdsTxt).catch(e => e),
+  fetchAndParse("/app-ads.txt", parseAdsTxt).catch(e => e),
+  fetchAndParse("/sellers.json", parseSellersJSON).catch(e => e),
 ]).then((all_data) => {
-	return JSON.stringify({
-		ads: all_data[0],
-		app_ads: all_data[1],
-		sellers: all_data[2]
-	});
+  return JSON.stringify({
+    ads: all_data[0],
+    app_ads: all_data[1],
+    sellers: all_data[2]
+  });
 }).catch(error => {
-	return JSON.stringify({
-		error: error.message
-	});
+  return JSON.stringify({
+    error: error.message
+  });
 });
--- dist/privacy-sandbox.js
+++ dist/privacy-sandbox.js
@@ -7,84 +7,84 @@ const testURL = new URL($WPT_URL);
 let firstPartyDomain = testURL.hostname;
 
 result = {
-	topicsAvailable: document.featurePolicy.allowsFeature('browsing-topics'),
-	thirdPartiesUsingBrowsingTopics: {}
+  topicsAvailable: document.featurePolicy.allowsFeature('browsing-topics'),
+  thirdPartiesUsingBrowsingTopics: {}
 }
 
 function fetchAndCheckAttestation(url) {
-	const controller = new AbortController();
-	const { signal } = controller;
-	setTimeout(() => controller.abort(), 5000);
+  const controller = new AbortController();
+  const { signal } = controller;
+  setTimeout(() => controller.abort(), 5000);
 
-	try {
-		const response = fetch(url, { signal });
-		if (response.ok && response.headers.get('Content-Type').includes('application/json')) {
-			return true;
-		}
-		else {
-			return false;
-		}
-	} catch (error) {
-		return false;
-	}
+  try {
+    const response = fetch(url, { signal });
+    if (response.ok && response.headers.get('Content-Type').includes('application/json')) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  } catch (error) {
+    return false;
+  }
 }
 
 // Checks if the request corresponds to a JavaScript file
 function isJavaScript(requestUrl, trimmedURL, contentType) {
-	return (requestUrl.endsWith('.js') || trimmedURL.endsWith('.js') || contentType.includes('javascript'));
+  return (requestUrl.endsWith('.js') || trimmedURL.endsWith('.js') || contentType.includes('javascript'));
 }
 
 requests.forEach(request => {
-	const url = new URL(request.url);
-	const trimmedURL = url.origin + url.pathname;
-	const isScript = request.type === 'Script';
-	const isDocument = request.type === 'Document';
+  const url = new URL(request.url);
+  const trimmedURL = url.origin + url.pathname;
+  const isScript = request.type === 'Script';
+  const isDocument = request.type === 'Document';
 
-	let thirdPartyDomain = '';
-	if (url.hostname !== firstPartyDomain) {
-		thirdPartyDomain = url.hostname;
-	}
+  let thirdPartyDomain = '';
+  if (url.hostname !== firstPartyDomain) {
+    thirdPartyDomain = url.hostname;
+  }
 
-	let jsUsage = false;
-	let headerUsage = false;
-	let receiverObserving = false;
-	let attestation = false;
-	let tldPlus1Attestation = false;
+  let jsUsage = false;
+  let headerUsage = false;
+  let receiverObserving = false;
+  let attestation = false;
+  let tldPlus1Attestation = false;
 
-	// If the request is to fetch a javascript file or HTML document, perform string search in the returned file content for usage of Topics API
-	// document.browsingTopics() [JS] or fetch() request call includes: {browsingTopics: true} or XHR request call includes: {deprecatedBrowsingTopics: true}
-	if (isScript || isJavaScript(request.url, trimmedURL, request.response_headers['Content-Type']) || isDocument) {
-		if (request.response_body && (request.response_body.includes('browsingTopics') || request.response_body.includes('BrowsingTopics'))) {
-			jsUsage = true;
-		}
-	}
+  // If the request is to fetch a javascript file or HTML document, perform string search in the returned file content for usage of Topics API
+  // document.browsingTopics() [JS] or fetch() request call includes: {browsingTopics: true} or XHR request call includes: {deprecatedBrowsingTopics: true}
+  if (isScript || isJavaScript(request.url, trimmedURL, request.response_headers['Content-Type']) || isDocument) {
+    if (request.response_body && (request.response_body.includes('browsingTopics') || request.response_body.includes('BrowsingTopics'))) {
+      jsUsage = true;
+    }
+  }
 
-	// Checking request header usage of Topics: header: 'Sec-Browsing-Topics: true'
-	if (request.request_headers.some(header => header.name === 'Sec-Browsing-Topics')) {
-		headerUsage = true;
-		// Checking is sent Topics are observed by the receiver using response header 'Observe-Browsing-Topics'
-		// If value is ?1 then they are observed else they are not observed
-		if (request.response_headers.some(header => header.name === 'Observe-Browsing-Topics' && header.value === '?1')) {
-			receiverObserving = true;
-		}
-	}
+  // Checking request header usage of Topics: header: 'Sec-Browsing-Topics: true'
+  if (request.request_headers.some(header => header.name === 'Sec-Browsing-Topics')) {
+    headerUsage = true;
+    // Checking is sent Topics are observed by the receiver using response header 'Observe-Browsing-Topics'
+    // If value is ?1 then they are observed else they are not observed
+    if (request.response_headers.some(header => header.name === 'Observe-Browsing-Topics' && header.value === '?1')) {
+      receiverObserving = true;
+    }
+  }
 
-	// Checking if the third party has published an attestation for Privacy Sandbox
-	if (thirdPartyDomain) {
-		attestation = fetchAndCheckAttestation(`${url.origin}/.well-known/privacy-sandbox-attestations.json`);
-		tldPlus1Origin = 'https://' + url.hostname.split('.').slice(-2).join('.');
-		tldPlus1Attestation = fetchAndCheckAttestation(`${tldPlus1Origin}/.well-known/privacy-sandbox-attestations.json`);
-	}
+  // Checking if the third party has published an attestation for Privacy Sandbox
+  if (thirdPartyDomain) {
+    attestation = fetchAndCheckAttestation(`${url.origin}/.well-known/privacy-sandbox-attestations.json`);
+    tldPlus1Origin = 'https://' + url.hostname.split('.').slice(-2).join('.');
+    tldPlus1Attestation = fetchAndCheckAttestation(`${tldPlus1Origin}/.well-known/privacy-sandbox-attestations.json`);
+  }
 
-	if (thirdPartyDomain && (jsUsage || headerUsage || attestationPublished)) {
-		result.thirdPartiesUsingBrowsingTopics[thirdPartyDomain] = {
-			topicsAccessJs: jsUsage,
-			topicsAccessHeader: headerUsage,
-			observingTopics: receiverObserving,
-			attestationPublished: attestation,
-			tldPlus1AttestationPublished: tldPlus1Attestation
-		};
-	}
+  if (thirdPartyDomain && (jsUsage || headerUsage || attestationPublished)) {
+    result.thirdPartiesUsingBrowsingTopics[thirdPartyDomain] = {
+      topicsAccessJs: jsUsage,
+      topicsAccessHeader: headerUsage,
+      observingTopics: receiverObserving,
+      attestationPublished: attestation,
+      tldPlus1AttestationPublished: tldPlus1Attestation
+    };
+  }
 
 });
 
