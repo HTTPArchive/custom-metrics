d56f339ee03ecf98321a14d1031fe5b2c997c369
Yash Vekaria <yvekaria.09@gmail.com>
2024-05-23 18:09:48 -0700

    Simplified the implementation

--- dist/privacy-sandbox.js
+++ dist/privacy-sandbox.js
@@ -3,89 +3,76 @@
 // Header Usage Reference: https://developers.google.com/privacy-sandbox/relevance/topics/demo#the-topics-api-demo
 
 let requests = $WPT_BODIES;
-const testURL = new URL($WPT_URL);
-let firstPartyDomain = testURL.hostname;
+const firstPartyDomain = document.location.hostname;
 
-result = {
+let result = {
   topicsAvailable: document.featurePolicy.allowsFeature('browsing-topics'),
   thirdPartiesUsingBrowsingTopics: {}
 }
 
-function fetchAndCheckAttestation(url) {
+async function fetchAndCheckAttestation(url) {
   const controller = new AbortController();
   const { signal } = controller;
   setTimeout(() => controller.abort(), 5000);
 
   try {
-    const response = fetch(url, { signal });
-    if (response.ok && response.headers.get('Content-Type').includes('application/json')) {
-      return true;
-    }
-    else {
-      return false;
-    }
+    const response = await fetch(url, { signal, mode: 'no-cors' });
+    return response ? true : false;
   } catch (error) {
     return false;
   }
 }
 
-// Checks if the request corresponds to a JavaScript file
-function isJavaScript(requestUrl, trimmedURL, contentType) {
-  return (requestUrl.endsWith('.js') || trimmedURL.endsWith('.js') || contentType.includes('javascript'));
-}
+let seenThirdParties = [];
+(async () => {
+  for (const request of requests) {
+    const url = new URL(request.url);
+    const isScript = request.type === 'Script';
+    const isDocument = request.type === 'Document';
 
-requests.forEach(request => {
-  const url = new URL(request.url);
-  const trimmedURL = url.origin + url.pathname;
-  const isScript = request.type === 'Script';
-  const isDocument = request.type === 'Document';
-
-  let thirdPartyDomain = '';
-  if (url.hostname !== firstPartyDomain) {
-    thirdPartyDomain = url.hostname;
-  }
-
-  let jsUsage = false;
-  let headerUsage = false;
-  let receiverObserving = false;
-  let attestation = false;
-  let tldPlus1Attestation = false;
-
-  // If the request is to fetch a javascript file or HTML document, perform string search in the returned file content for usage of Topics API
-  // document.browsingTopics() [JS] or fetch() request call includes: {browsingTopics: true} or XHR request call includes: {deprecatedBrowsingTopics: true}
-  if (isScript || isJavaScript(request.url, trimmedURL, request.response_headers['Content-Type']) || isDocument) {
-    if (request.response_body && (request.response_body.includes('browsingTopics') || request.response_body.includes('BrowsingTopics'))) {
-      jsUsage = true;
+    let thirdPartyDomain = '';
+    if (url.hostname !== firstPartyDomain) {
+      thirdPartyDomain = url.hostname;
     }
-  }
-
-  // Checking request header usage of Topics: header: 'Sec-Browsing-Topics: true'
-  if (request.request_headers.some(header => header.name === 'Sec-Browsing-Topics')) {
-    headerUsage = true;
-    // Checking is sent Topics are observed by the receiver using response header 'Observe-Browsing-Topics'
-    // If value is ?1 then they are observed else they are not observed
-    if (request.response_headers.some(header => header.name === 'Observe-Browsing-Topics' && header.value === '?1')) {
-      receiverObserving = true;
+    if (thirdPartyDomain && seenThirdParties.includes(thirdPartyDomain)) {
+        continue;
     }
-  }
+    seenThirdParties.push(thirdPartyDomain);
 
-  // Checking if the third party has published an attestation for Privacy Sandbox
-  if (thirdPartyDomain) {
-    attestation = fetchAndCheckAttestation(`${url.origin}/.well-known/privacy-sandbox-attestations.json`);
-    tldPlus1Origin = 'https://' + url.hostname.split('.').slice(-2).join('.');
-    tldPlus1Attestation = fetchAndCheckAttestation(`${tldPlus1Origin}/.well-known/privacy-sandbox-attestations.json`);
-  }
+    let jsUsage = false;
+    let headerUsage = false;
+    let receiverObserving = false;
 
-  if (thirdPartyDomain && (jsUsage || headerUsage || attestationPublished)) {
-    result.thirdPartiesUsingBrowsingTopics[thirdPartyDomain] = {
-      topicsAccessJs: jsUsage,
-      topicsAccessHeader: headerUsage,
-      observingTopics: receiverObserving,
-      attestationPublished: attestation,
-      tldPlus1AttestationPublished: tldPlus1Attestation
-    };
-  }
+    // If the request is to fetch a javascript file or HTML document, perform string search in the returned file content for usage of Topics API
+    // document.browsingTopics() [JS] or fetch() request call includes: {browsingTopics: true} or XHR request call includes: {deprecatedBrowsingTopics: true}
+    if (isScript || isDocument) {
+      if (request.response_body && (request.response_body.includes('browsingTopics') || request.response_body.includes('BrowsingTopics'))) {
+        jsUsage = true;
+      }
+    }
 
-});
+    // Checking request header usage of Topics: header: 'Sec-Browsing-Topics: true'
+    if ('Sec-Browsing-Topics' in request.request_headers) {
+      headerUsage = true;
+      // Checking is sent Topics are observed by the receiver using response header 'Observe-Browsing-Topics'
+      // If value is ?1 then they are observed else they are not observed
+      if (request.response_headers['Observe-Browsing-Topics'] === '?1') {
+        receiverObserving = true;
+      }
+    }
+
+    if (thirdPartyDomain && (jsUsage || headerUsage)) {
+      // Checking if the third party has published an attestation for Privacy Sandbox
+      const tldPlus1Origin = 'https://' + url.hostname.split('.').slice(-2).join('.');
+      result.thirdPartiesUsingBrowsingTopics[thirdPartyDomain] = {
+        topicsAccessJs: jsUsage,
+        topicsAccessHeader: headerUsage,
+        observingTopics: receiverObserving,
+        attestationPublished: await fetchAndCheckAttestation(`${url.origin}/.well-known/privacy-sandbox-attestations.json`),
+        tldPlus1AttestationPublished: await fetchAndCheckAttestation(`${tldPlus1Origin}/.well-known/privacy-sandbox-attestations.json`)
+      };
+    }
+  };
+})();
 
 return result;
