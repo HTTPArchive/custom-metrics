265c5a7233afd9871f520616e2e581953c8bf493
Shaoor Munir <shaoormunir@gmail.com>
2024-06-07 00:05:34 -0700

    updated to only account for API calls and not for checking the permissions and object assignments

--- dist/local_storage_api_calls.js
+++ dist/local_storage_api_calls.js
@@ -1,18 +1,23 @@
 const response_bodies = $WPT_BODIES;
-const storagePattern = /\b(localStorage|sessionStorage)\b/g;
+const storagePattern = /\b(localStorage|sessionStorage)\b(\.\w+)?/g;
 
 return Object.fromEntries(response_bodies.filter(har => {
   return storagePattern.test(har.response_body);
 }).map(har => {
   const matches = Array.from(har.response_body.matchAll(storagePattern));
   const counts = matches.reduce((acc, match) => {
-    const key = match[0];
-    if (!acc[key]) {
-      acc[key] = 0;
+    const key = match[1];
+    const access = match[2];
+
+    if (access && (access.startsWith('.getItem') || access.startsWith('.setItem') || access.startsWith('.removeItem'))) {
+      if (!acc[key]) {
+        acc[key] = 0;
+      }
+      acc[key]++;
     }
-    acc[key]++;
+
     return acc;
-  }, { numLocalStorage: 0, numSessionStorage: 0 });
+  }, { localStorage: 0, sessionStorage: 0 });
 
   return [har.url, { numLocalStorage: counts.localStorage || 0, numSessionStorage: counts.sessionStorage || 0 }];
 }));
