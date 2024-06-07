4f05ee03827872949da87e11a52c048462f4c7a1
Shaoor Munir <shaoormunir@gmail.com>
2024-06-06 23:49:22 -0700

    limited the value of each local storage item to first 256 bytes

--- dist/local_session_storage_key_values.js
+++ dist/local_session_storage_key_values.js
@@ -1,9 +1,19 @@
 function getLocalStorageKeyValues() {
-  return { ...localStorage };
+  return Object.keys(localStorage)
+    .reduce((obj, key) => {
+      const value = localStorage.getItem(key);
+      obj[key] = value ? value.substring(0, 256) : value;
+      return obj;
+    }, {});
 }
 
 function getSessionStorageKeyValues() {
-  return { ...sessionStorage };
+  return Object.keys(sessionStorage)
+    .reduce((obj, key) => {
+      const value = sessionStorage.getItem(key);
+      obj[key] = value ? value.substring(0, 256) : value;
+      return obj;
+    }, {});
 }
 
 const storageData = {
