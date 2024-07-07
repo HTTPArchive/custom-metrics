7396be485e7e7d29835d3a4265656638936c8841
Max Ostapenko <1611259+max-ostapenko@users.noreply.github.com>
2024-07-07 23:50:44 +0200

    test update

--- tests/wpt.js
+++ tests/wpt.js
@@ -1,123 +1,177 @@
 const WebPageTest = require('webpagetest');
 const fs = require('fs');
 const path = require('path');
-const { argv } = require('node:process');
 const { execSync } = require('child_process');
 
-const is_direct_run = require.main === module;
-
-const wptServer = process.env.WPT_SERVER;
-const wptApiKey = process.env.WPT_API_KEY;
-const wpt = new WebPageTest(wptServer, wptApiKey);
-
-
-/**
- * Retrieves the names of all JavaScript files in the 'dist/' directory.
- *
- * @returns {string[]} An array of the base names of the JavaScript files without the '.js' extension.
- */
-function getCustomMetrics() {
-  return fs.readdirSync('dist/', { withFileTypes: true })
-    .filter(file => path.extname(file.name) === '.js')
-    .map(file => path.basename(file.name, '.js'));
+function setGithubActionOutput(name, value) {
+  const githubOutputFile = process.env.GITHUB_OUTPUT;
+  if (githubOutputFile) {
+    const outputString = `${name}=${value}\n`;
+    fs.appendFile(githubOutputFile, outputString);
+  }
 }
 
-
-/**
- * Retrieves the names of all JavaScript files in the 'dist/' directory that have changed in the current branch.
- *
- * @returns {string[]} An array of the base names of the JavaScript files without the '.js' extension.
- */
-function getChangedCustomMetrics() {
-  const stdout = execSync('git diff --name-only --diff-filter=ACMRT origin/main', { encoding: 'utf-8' })
-
-  metricsList = stdout.split('\n')
-    .filter(file => RegExp('^dist\/.*\.js$', 'g').test(file))
-    .map(file => path.basename(file, '.js'))
-
-  metricsList = Array.from(new Set(metricsList)).sort()
-
-  return metricsList;
-}
-
-
-/**
- * Runs a WebPageTest (WPT) test for a given URL.
- *
- * @param {string} url - The URL to run the test on.
- * @returns {Promise<object>} A promise that resolves with an object containing the custom metrics.
- * @throws {Error} If the test run fails or the response status code is not 200.
- */
-function runWPTTest(url) {
-  const custom_metrics = getCustomMetrics()
-  const metrics_to_log = getChangedCustomMetrics()
-
-  let options = { key: wptApiKey, custom: '' };
-  for (const metric_name of custom_metrics) {
-    options.custom += `[_${metric_name}]\n` + fs.readFileSync(`./dist/${metric_name}.js`, 'utf-8') + `\n`;
+class WPTTestRunner {
+  constructor(prBody, wptServer, wptApiKey) {
+    this.prBody = prBody || '';
+    this.wpt = new WebPageTest(wptServer, wptApiKey);
+    this.testResultsFile = 'comment.md';
+    this.uploadArtifact = false;
   }
 
-  console.log(`WPT test run for ${url} started`);
-  return new Promise((resolve, reject) => {
-    wpt.runTestAndWait(url, options, (error, response) => {
-      if (error || response.statusCode !== 200) {
-        console.error(`WPT test run for ${url} failed:`);
-        console.error(error || response);
-        reject(error || response);
+  getCustomMetrics() {
+    return fs.readdirSync('dist/', { withFileTypes: true })
+      .filter(file => path.extname(file.name) === '.js')
+      .map(file => path.basename(file.name, '.js'));
+  }
 
-      } else {
-        console.log(`WPT test run for ${url} completed`);
-        let wpt_custom_metrics = {}
-        let wpt_custom_metrics_to_log = {}
+  getChangedCustomMetrics() {
+    const stdout = execSync('git diff --name-only --diff-filter=ACMRT origin/main', { encoding: 'utf-8' });
+    return Array.from(new Set(stdout.split('\n')
+      .filter(file => /^dist\/.*\.js$/.test(file))
+      .map(file => path.basename(file, '.js'))))
+      .sort();
+  }
 
-        for (const metric_name of custom_metrics) {
-          let wpt_custom_metric = response.data.runs['1'].firstView[`_${metric_name}`];
-          try {
-            // Some, but not all, custom metrics wrap their return values in JSON.stringify()
-            // Some just return the objects. And some have strings which are not JSON!
-            // Gotta love the consistency!!!
-            //
-            // IMHO wrapping the return in JSON.stringify() is best practice, since WebPageTest
-            // will do that to save to database and you can run into weird edge cases where it
-            // doesn't return data when doing that, so explicitly doing that avoids that when
-            // testing in the console* , but we live in an inconsistent world!
-            // (* see https://github.com/HTTPArchive/custom-metrics/pull/113#issuecomment-2043937823)
-            //
-            // Anyway, if it's a string, see if we can parse it back to a JS object to pretty
-            // print it, but be prepared for that to fail for non-JSON strings so wrap in try/catch
-            if (typeof wpt_custom_metric === 'string') {
-              wpt_custom_metric = JSON.parse(wpt_custom_metric);
-            }
-          } catch (e) {
-            // If it fails, that's OK as we'll just stick with the exact string output anyway
-          }
-          wpt_custom_metrics[`_${metric_name}`] = wpt_custom_metric;
-          if (metrics_to_log.includes(metric_name)) {
-            wpt_custom_metrics_to_log[`_${metric_name}`] = wpt_custom_metric;
-          }
-        }
+  checkCommentSize(stringLength) {
+    let commentSize = 0;
+    try {
+      commentSize = fs.statSync(testResultsFile).size;
+    } catch (err) { }
 
-        fs.appendFileSync('test-results.md', `<details>
+    if (commentSize + stringLength > 65536) {
+      const artifactFile = 'artifact.md';
+      this.uploadArtifact = true;
+      if (commentSize > 0) {
+        fs.renameSync(this.testResultsFile, artifactFile);
+      }
+      fs.appendFileSync(this.testResultsFile, `Test results are too big for a comment, and are available in the action artifact.`);
+      this.testResultsFile = artifactFile;
+    }
+  }
+
+  async runWPTTest(url) {
+    console.log(`::group::WPT test run for ${url} started`);
+    const customMetrics = this.getCustomMetrics();
+    const metricsToLog = this.getChangedCustomMetrics();
+    const options = { key: this.wpt.apiKey, http_method: 'POST', custom: '' };
+
+    customMetrics.forEach(metricName => {
+      options.custom += `[_${metricName}]\n${fs.readFileSync(`./dist/${metricName}.js`, 'utf-8')}\n`;
+    });
+
+    try {
+      const response = await this.runTestAndWait(url, options);
+
+      if (response.statusCode !== 200) {
+        throw new Error(`WPT test run for ${url} failed: ${response.statusText}`);
+      }
+
+      const metricsToLogString = JSON.stringify(
+        this.extractMetrics(response.data.runs['1'].firstView, customMetrics, metricsToLog), null, 2
+      );
+
+      if (!this.uploadArtifact) {
+        this.checkCommentSize(metricsToLogString.length);
+      }
+
+      fs.appendFileSync(this.testResultsFile, `<details>
 <summary><strong>Custom metrics for ${url}</strong></summary>
 
 WPT test run results: ${response.data.summary}
 Changed custom metrics values:
 \`\`\`json
-${JSON.stringify(wpt_custom_metrics_to_log, null, 4)}
+${metricsToLogString}
 \`\`\`
 </details>\n\n`);
 
-        resolve(wpt_custom_metrics);
+      console.log('::endgroup::');
+    } catch (error) {
+      console.error(`WPT test run for ${url} failed:`, error);
+    }
+  }
+
+  runTestAndWait(url, options) {
+    return new Promise((resolve, reject) => {
+      this.wpt.runTestAndWait(url, options, (error, response) => {
+        if (error || response.statusCode !== 200) {
+          reject(error || response);
+        } else {
+          resolve(response);
+        }
+      });
+    });
+  }
+
+  extractMetrics(firstViewData, customMetrics, metricsToLog) {
+    const wptCustomMetrics = {};
+    const wptCustomMetricsToLog = {};
+
+    customMetrics.forEach(metricName => {
+      let wptCustomMetric = firstViewData[`_${metricName}`];
+      try {
+        if (typeof wptCustomMetric === 'string') {
+          wptCustomMetric = JSON.parse(wptCustomMetric);
+        }
+      } catch (e) { }
+
+      wptCustomMetrics[`_${metricName}`] = wptCustomMetric;
+      if (metricsToLog.includes(metricName)) {
+        wptCustomMetricsToLog[`_${metricName}`] = wptCustomMetric;
       }
     });
-  });
+
+    return wptCustomMetricsToLog;
+  }
+
+  getTestWebsites() {
+    const urlPattern = /((http|https):\/\/[a-zA-Z0-9.-]+(\.[a-zA-Z]{2,4})(\/[a-zA-Z0-9_.-]+)*(\/?)(\?[a-zA-Z0-9_.-]+=[a-zA-Z0-9%_.-]+)*(\#?)([a-zA-Z0-9%_.-=]+)*)/;
+    if (!this.prBody) {
+      console.log('No PR description body found.');
+      return [];
+    }
+    return this.prBody.split(/\r?\n/).reduce((urls, line, index, lines) => {
+      if (line.includes('**Test websites**:')) {
+        for (let i = index + 1; i < lines.length; i++) {
+          const match = lines[i].match(urlPattern);
+          if (match) {
+            urls.push(match[1]);
+          }
+        }
+      }
+      if (urls.length = 0) {
+        console.log('No test websites found in PR description body.');
+      }
+      return urls;
+    }, []);
+  }
+
+  async runTests() {
+    const urls = this.getTestWebsites();
+    if (urls.length > 0) {
+      for (const url of urls) {
+        await this.runWPTTest(url);
+      }
+    }
+  }
 }
 
+function main() {
+  const prBody = process.env.PR_BODY || '';
+  const wptServer = process.env.WPT_SERVER;
+  const wptApiKey = process.env.WPT_API_KEY;
 
-if (is_direct_run) {
-  const url = argv[2];
+  const runner = new WPTTestRunner(prBody, wptServer, wptApiKey);
+  runner.runTests();
 
-  runWPTTest(url);
+  const url = process.argv[2];
+  if (url) {
+    runner.runWPTTest(url);
+  }
 }
 
-module.exports = { runWPTTest };
+if (require.main === module) {
+  main();
+}
+
+module.exports = { WPTTestRunner };
