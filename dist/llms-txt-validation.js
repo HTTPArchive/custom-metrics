dc9ba7af312f1778deccb1641719342a7a92c302
Barry Pollard <barrypollard@google.com>
2025-06-21 21:03:51 +0100

    Update llms-txt-validation.js

--- dist/llms-txt-validation.js
+++ dist/llms-txt-validation.js
@@ -1,16 +1,23 @@
 //[llms-txt-valid]
-return fetch('/llms.txt')
+
+const fetchWithTimeout = (url) => {
+  var controller = new AbortController();
+  setTimeout(() => {controller.abort()}, 5000);
+  return fetch(url, {signal: controller.signal});
+}
+
+return fetchWithTimeout('/llms.txt')
   .then(response => {
-    if (!response.ok) return JSON.stringify({valid:0, error: "Non OK status code"});
+    if (!response.ok) return JSON.stringify({valid:0, message:response.status, error: "Non OK status code"});
     const ct = response.headers.get('Content-Type')||'';
     if (!ct.toLowerCase().includes('text/plain')) return JSON.stringify({valid:0,error: "Invalid content type"});
     return response.text().then(text => {
       const m = s=> (text.match(new RegExp(`\\${s}`,'g'))||[]).length;
-      if ((text.match(/```/g)||[]).length %2) return JSON.stringify({valid:0,error:""});
-      if (m('[')!==m(']')||m('(')!==m(')')) return JSON.stringify({valid:0,error:""});
+      if ((text.match(/```/g)||[]).length %2) return JSON.stringify({valid:0, error:"Invalid markdown fences"});
+      if (m('[')!==m(']')||m('(')!==m(')')) return JSON.stringify({valid:0, error:"Unmatch braces"});
       return JSON.stringify({valid:1});
     });
   })
   .catch(error => {
-    return JSON.stringify({valid:0,message: error.message, error: error});
+    return JSON.stringify({valid:0, message: error.message, error: error});
   });
