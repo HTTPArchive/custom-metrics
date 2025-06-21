c9f93aed85cec5c1b7379548a83621924879b07d
Barry Pollard <barrypollard@google.com>
2025-06-21 20:15:36 +0100

    JSON stringify

--- llms-txt-validation.js
+++ llms-txt-validation.js
@@ -1,16 +1,16 @@
 //[llms-txt-valid]
 return fetch('/llms.txt')
   .then(response => {
-    if (!response.ok) return {"exists":0,"valid":0};
+    if (!response.ok) return JSON.stringify({valid:0, error: "Non OK status code"});
     const ct = response.headers.get('Content-Type')||'';
-    if (!ct.toLowerCase().includes('text/plain')) return {"exists":1,"valid":0};
+    if (!ct.toLowerCase().includes('text/plain')) return JSON.stringify({valid:0,error: "Invalid content type"});
     return response.text().then(text => {
       const m = s=> (text.match(new RegExp(`\\${s}`,'g'))||[]).length;
-      if ((text.match(/```/g)||[]).length %2) return {"exists":1,"valid":0};
-      if (m('[')!==m(']')||m('(')!==m(')')) return {"exists":1,"valid":0};
-      return {"exists":1,"valid":1};
+      if ((text.match(/```/g)||[]).length %2) return JSON.stringify({valid:0,error:""});
+      if (m('[')!==m(']')||m('(')!==m(')')) return JSON.stringify({valid:0,error:""});
+      return JSON.stringify({valid":1});
     });
   })
   .catch(error => {
-    return JSON.stringify({message: error.message, error: error});
+    return JSON.stringify({valid:0,message: error.message, error: error});
   });
